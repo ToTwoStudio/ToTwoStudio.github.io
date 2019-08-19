08982ef64dbfc66f54f80e71d9b810dbe43eb8f0
HTRTan <29402158+HTRTan@users.noreply.github.com>
2019-08-20 01:36:50 +0800

    ToTwoWebsite

---
+++ js/main.js
@@ -0,0 +1,111 @@
+
+jQuery(document).ready(function($) {
+
+	'use strict';
+
+    var top_header = $('.parallax-content');
+    top_header.css({'background-position':'center center'}); // better use CSS
+
+    $(window).scroll(function () {
+    var st = $(this).scrollTop();
+    top_header.css({'background-position':'center calc(50% + '+(st*.5)+'px)'});
+    });
+
+
+    $('body').scrollspy({ 
+        target: '.fixed-side-navbar',
+        offset: 200
+    });
+
+    $('.tabgroup > div').hide();
+        $('.tabgroup > div:first-of-type').show();
+        $('.tabs a').click(function(e){
+          e.preventDefault();
+            var $this = $(this),
+            tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
+            others = $this.closest('li').siblings().children('a'),
+            target = $this.attr('href');
+        others.removeClass('active');
+        $this.addClass('active');
+        $(tabgroup).children('div').hide();
+        $(target).show();
+      
+    })
+
+    var owl = $("#owl-testimonials");
+
+      owl.owlCarousel({
+        
+        pagination : true,
+        paginationNumbers: false,
+        autoPlay: 6000,
+        items : 3,
+        itemsDesktop : [1000,3],
+        itemsDesktopSmall : [900,2],
+        itemsTablet: [600,1],
+        itemsMobile : false
+        
+    });
+
+    try{
+			window.console&&window.console.log&&(console.log("\n\n同学\n这里有你想追求的信念\n来这里做最初的自己\n不负你我\n未来路上，我们风雨同路"),console.log("%cToTwoStudio欢迎你：http://www.totwostudio.com \n报名： %c请在网页中寻找小姐姐微信\n或 发邮件至ToTwoStudio@yeah.net\n","color:blue","color:red"))
+		}catch(e){} 
+
+
+});
+
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 1000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 150 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.02em solid #fff}";
+  document.body.appendChild(css);
+};  
